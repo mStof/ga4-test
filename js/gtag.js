7904e83a829921e4164b7ed587dc022bad282826
Mstof <mateus.stofel.brito@gmail.com>
2026-09-16 00:57:00 -0300

    adding gtag (page_view)

--- js/gtag.js
+++ js/gtag.js
@@ -6,3 +6,12 @@ dataLayer.push(arguments);
 gtag('js', new Date());
 
 gtag('config', 'G-E8R8LKVKFL');
+
+gtag('event', 'page_view', {
+    page_location: window.location.href,
+    client_id: "ID_test",
+    language: navigator.language,
+    page_enconding: document.characterSet,
+    page_title: document.title,
+
+})
